66abc2896bea5555cffc8fc30b3e72273fbafaba
Shruti Mandaokar <shruti.mandaokar@gmail.com>
2023-05-25 22:33:57 +0530

    Create Kids With the Greatest Number of Candies.py

---
+++ Kids With the Greatest Number of Candies.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maximum = max(candies)
+        result = []
+        for a in candies:
+            if a + extraCandies >= maximum:
+                result.append(True)
+            else:
+                result.append(False)
+        return result
