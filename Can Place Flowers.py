62fdb8be2b18abc350b91ac179c545ae3b0eca69
Shruti Mandaokar <shruti.mandaokar@gmail.com>
2023-05-26 21:34:35 +0530

    Create Can Place Flowers.py

---
+++ Can Place Flowers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        x=y=0
+        flowerbe=[0]+flowerbed+[0]+[1]
+        for i in flowerbe:
+            if i==0:
+                x=x+1
+            elif x>=2 and x%2==0:
+                y=y+(x//2)-1
+                x=0
+            elif x>=3 and x%2!=0:
+                y=y+(x//2)
+                x=0
+            else:
+                x=0
+        if y>=n:
+            return True
+        return False
